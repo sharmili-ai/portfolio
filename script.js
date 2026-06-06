5d17ac63c3d93eff7b14bea29933d44866b84b9b
Sharmili-AI <sharmilipraba1706@gmail.com>
2026-06-06 18:34:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Portfolio Loaded Successfully 🚀");
+const text = "I build AI systems that solve real-world problems.";
+let i = 0;
+
+function type() {
+    if (i < text.length) {
+        document.querySelector(".typing").textContent += text.charAt(i);
+        i++;
+        setTimeout(type, 50);
+    }
+}
+
+document.querySelector(".typing").textContent = "";
+type();
